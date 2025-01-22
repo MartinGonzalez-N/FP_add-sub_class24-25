83b8944ac2c531c25d88f720e7c325c7deb86a11
DanielGG50 <daniel.garcia@cinvestav.mx>
2025-01-22 12:57:03 -0600

    Poner vaiables
    Se añaden las entreadas, la salida y se separan las entradas en las 3 partes que conforman el FP

--- FP_add-sub_class24-25/FP_add-sub_class24-25.srcs/sources_1/new/add_sub.sv
+++ FP_add-sub_class24-25/FP_add-sub_class24-25.srcs/sources_1/new/add_sub.sv
@@ -1,15 +1,15 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
+// Company: Cinvestav
+// Engineer: Dabiel, Lino, Kevin, Emmanuel
 // 
 // Create Date: 22.01.2025 12:31:55
-// Design Name: 
+// Design Name: Floating Point Adder and Subctractor
 // Module Name: add_sub
-// Project Name: 
+// Project Name: Floating Point Adder and Subctractor
 // Target Devices: 
 // Tool Versions: 
-// Description: 
+// Description: Adds and subtracts two inputs and delivers one outup following IEEE 754 format
 // 
 // Dependencies: 
 // 
@@ -20,7 +20,21 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module add_sub(
-
+module add_sub #(parameter WIDTH = 32)(
+    input  [WIDTH-1:0] a,b,
+    output [WIDTH-1:0] result,
+    output sign_a, sign_b, sign_result,
+    output [WIDTH-10:0] mantissa_a, mantissa_b, mantissa_result,
+    output [WIDTH-25:0] exp_a, exp_b, exp_result
     );
+    
+    //Separate sign, exponent and mantissa values for both inputs
+    assign mantissa_a = a[22:0];
+    assign exp_a = a[30:23];
+    assign sing_a = a[31];
+        
+    assign mantissa_b = b[22:0];
+    assign exp_b = b[30:23];
+    assign sign_b = b[31];
+    
 endmodule
