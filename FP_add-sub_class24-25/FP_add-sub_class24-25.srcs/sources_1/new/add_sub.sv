91ac0bb3992c2d3943389ffef51cd738a6a5e7bf
KevAraujo <kevin.araujo@cinvestav.mx>
2025-01-23 15:28:35 -0600

    Prueba 2

--- FP_add-sub_class24-25/FP_add-sub_class24-25.srcs/sources_1/new/add_sub.sv
+++ FP_add-sub_class24-25/FP_add-sub_class24-25.srcs/sources_1/new/add_sub.sv
@@ -19,7 +19,7 @@
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
-
+Prueba 2
 module add_sub #(parameter WIDTH = 32)(
     input  [WIDTH-1:0] a,b,
     input bit operation_select,
