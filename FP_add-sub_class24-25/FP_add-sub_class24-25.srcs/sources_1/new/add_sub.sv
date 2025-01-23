34b53f0500b307323a2cbab5e6a5368ae2666963
KevAraujo <kevin.araujo@cinvestav.mx>
2025-01-23 15:18:24 -0600

    Update add_sub.sv

--- FP_add-sub_class24-25/FP_add-sub_class24-25.srcs/sources_1/new/add_sub.sv
+++ FP_add-sub_class24-25/FP_add-sub_class24-25.srcs/sources_1/new/add_sub.sv
@@ -18,7 +18,6 @@
 // Additional Comments:
 // 
 //////////////////////////////////////////////////////////////////////////////////
-Prueba raama
 
 module add_sub #(parameter WIDTH = 32)(
     input  [WIDTH-1:0] a,b,
