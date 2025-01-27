5570bc2f73c690305dab90f54be49e2b18c13a90
KevAraujo <kevin.araujo@cinvestav.mx>
2025-01-27 01:18:41 -0600

    Update and rename add_sub.sv to sign_log.sv

--- FP_add-sub_class24-25/FP_add-sub_class24-25.srcs/sources_1/new/sign_log.sv
+++ FP_add-sub_class24-25/FP_add-sub_class24-25.srcs/sources_1/new/sign_log.sv
@@ -19,23 +19,32 @@
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
-Prueba 2
-module add_sub #(parameter WIDTH = 32)(
-    input  [WIDTH-1:0] a,b,
+module sign_log #(parameter WIDTH = 32)(
+    input wire [WIDTH-1:0] X,Y,
     input bit operation_select,
-    output [WIDTH-1:0] result,
-    output sign_a, sign_b, sign_result,
-    output [WIDTH-10:0] mantissa_a, mantissa_b, mantissa_result,
-    output [WIDTH-25:0] exp_a, exp_b, exp_result
-    );
+    //input bit clk,
+    output reg [0:0] result,
+    output sign_r
     
-    //Separate sign, exponent and mantissa values for both inputs
-    assign mantissa_a = a[22:0];
-    assign exp_a = a[30:23];
-    assign sign_a = a[31];
+    );
+    wire [22:0] mantissa_a, mantissa_b, mantissa_result;
+    wire sign_a, sign_b;
+    wire def1_1;
+    wire sign_x, sign_y;
+    
+    assign sign_x = X[31];
+    assign sign_y = Y[31];
+    
+    assign mantisa_a = X[22:0];
+    assign exp_a = X[30:23];
         
-    assign mantissa_b = b[22:0];
-    assign exp_b = b[30:23];
-    assign sign_b = b[31];
+    assign mantisa_b = Y[22:0];
+    assign exp_b = Y[30:23];
+    assign sign_b = operation_select ? sign_y : ~sign_y;
+    
+    assign def1_1 = (mantisa_a > mantisa_b) ? sign_x : (mantisa_a < mantisa_b) ? sign_b : (sign_a ~^ sign_b);
+    assign sign_r = (exp_a > exp_b) ? sign_x : (exp_a < exp_b) ? sign_b : def1_1;
+    
+    
     
 endmodule
