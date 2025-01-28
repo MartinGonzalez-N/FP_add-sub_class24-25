018eb6c9c6418af9662b790dd449c0bf812827c6
chutsE <jesus.esparza@cinvestav.mx>
2025-01-28 21:35:59 +0000

    added rtl_verif_work

---
+++ work/list.f
@@ -0,0 +1,10 @@
+# agregar en este archivo la ruta desde el directorio work hacia los
+# archivos de rtl y testbenches.
+#
+# ejemplo
+# ../rtl/adder.sv
+#
+# donde
+# .. es regresarse un directorio hacia atras
+# rtl es la carpeta donde esta el rtl
+# adder.sv es el archivo que se compilara 
