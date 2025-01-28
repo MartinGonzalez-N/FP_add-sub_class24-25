b06308053954304d95e89793490730d374280449
Lino-29 <lino_voxter@hotmail.com>
2025-01-28 23:11:31 +0000

    Modified list.f

--- work/list.f
+++ work/list.f
@@ -9,5 +9,10 @@
 # rtl es la carpeta donde esta el rtl
 # adder.sv es el archivo que se compilara 
 # Yevin se la come
-
+/home/disdig/dd_C1_25/add_sub_c125/FP_add-sub_class24-25/rtl/normalize_rounder.sv
+/home/disdig/dd_C1_25/add_sub_c125/FP_add-sub_class24-25/rtl/mantissa_add_sub
+/home/disdig/dd_C1_25/add_sub_c125/FP_add-sub_class24-25/rtl/mantissa_shifter
+/home/disdig/dd_C1_25/add_sub_c125/FP_add-sub_class24-25/rtl/exponent_logic
+/home/disdig/dd_C1_25/add_sub_c125/FP_add-sub_class24-25/rtl/sign_logic
+/home/disdig/dd_C1_25/add_sub_c125/FP_add-sub_class24-25/rtl/add_sub_main.sv
 
