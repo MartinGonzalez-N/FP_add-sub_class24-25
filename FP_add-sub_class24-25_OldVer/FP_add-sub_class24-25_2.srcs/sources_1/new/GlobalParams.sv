26153db49483607e5f55201f1378797c8532a49c
Emmanuel Díaz <emmanueldiazmarin@outlook.es>
2025-01-28 14:43:15 -0600

    Reinicio

--- FP_add-sub_class24-25_OldVer/FP_add-sub_class24-25_2.srcs/sources_1/new/GlobalParams.sv
+++
@@ -1,5 +0,0 @@
-package GlobalParams;
-    parameter int MANT_BITS = 23;
-    parameter int EXP_BITS   = 8;  // Bits del exponente
-    parameter int BIAS       = 127; // Sesgo del exponente para formato simple
-endpackage
