235cfd6a643ed48940a97b4e753b700bf3657acd
Lino-29 <lino_voxter@hotmail.com>
2025-01-28 23:40:48 +0000

    rtl changes

---
+++ work/xcelium.d/run.lnx8664.23.09.d/files.ts
@@ -0,0 +1,6 @@
+1738105195 /home/disdig/dd_C1_25/add_sub_c125/FP_add-sub_class24-25/rtl/normalize_rounder.sv
+1738107195 /home/disdig/dd_C1_25/add_sub_c125/FP_add-sub_class24-25/rtl/mantissa_add_sub.sv
+1738107195 /home/disdig/dd_C1_25/add_sub_c125/FP_add-sub_class24-25/rtl/mantissa_shifter.sv
+1738105195 /home/disdig/dd_C1_25/add_sub_c125/FP_add-sub_class24-25/rtl/exponent_sub_upd.sv
+1738105195 /home/disdig/dd_C1_25/add_sub_c125/FP_add-sub_class24-25/rtl/sign_logic.sv
+1738105195 /home/disdig/dd_C1_25/add_sub_c125/FP_add-sub_class24-25/rtl/add_sub_main.sv
