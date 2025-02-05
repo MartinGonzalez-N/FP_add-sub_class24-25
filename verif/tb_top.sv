1c79e1da22dd2b3e712d3f373acca0853ec61932
Chutse <jesusesparza242@gmail.com>
2025-02-05 22:24:05 +0000

    creating interface.sv file

--- verif/tb_top.sv
+++ verif/tb_top.sv
@@ -29,7 +29,7 @@ module tb_top;
         $shm_open("shm_db");
         $shm_probe("ASMTR");
     end
-    
+
     // Instanciar la interface
     add_sub_main_if #(.WIDTH(WIDTH)) add_sub_main_if_inst();
 
@@ -55,22 +55,3 @@ module tb_top;
    `endif
 
 endmodule
-
-interface add_sub_main_if #(parameter WIDTH = 32);
-    bit  [WIDTH-1:0] a,b;
-    bit operation_select;
-    bit clk;
-    logic [WIDTH-1:0] result;
-    logic sign_a, sign_b, sign_result;
-    logic [MANT_BITS-1:0] mantissa_a, mantissa_b, mantissa_result;
-    logic [EXP_BITS-1:0] exp_a, exp_b, exp_result;
-    
-
-    task task_generate_random_stimul();
-        begin
-            std::randomize(a);
-            std::randomize(b);
-            std::randomize(operation_select);
-        end
-    endtask
-endinterface 
